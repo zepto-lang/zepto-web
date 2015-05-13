060b170880737667101bed3283ec85ca9034c3d8
hellerve <veit.heller@student.htw-berlin.de>
2015-05-13 16:50:54 +0200

    Updated wiki and redirections

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/wiki', function(req, res) { res.redirect('https://github.com/zepto-lang/zepto/wiki'); });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
