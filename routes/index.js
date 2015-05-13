4de45923aadc543a02d57ff8ae0be86c654410b3
hellerve <veit.heller@student.htw-berlin.de>
2015-05-13 13:42:17 +0000

    Updated and added wiki

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Zepto' });
 });
 
+/* GET wiki. */
+router.get('/wiki', function(req, res, next) {
+  res.redirect('https://github.com/zepto-lang/zepto/wiki');
+});
+
 module.exports = router;
