e6a703bc7f1b6489fedf3b1425e139a0c1161ad7
hellerve <veit.heller@student.htw-berlin.de>
2015-05-21 17:23:14 +0200

    added transition to nav

--- public/javascript/wiki.js
+++ public/javascript/wiki.js
@@ -1,5 +1,5 @@
 var displayNav = function() {
   var el = document.getElementById('nav').style;
-  if (el.display == 'block') el.display = 'none';
-  else el.display = 'block';
+  if (el.width == '200px') { el.width = '0'; el.padding = '0'; }
+  else { el.width = '200px'; el.padding = '0.8em'; }
 }
